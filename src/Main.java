74c73d4fd1a6cc3637b2284056210dca0c4c0290
Kolorsik <roman.kukla@mail.ru>
2019-02-18 20:53:23 +0300

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        //TODO add a new method
+        //TODO add a new method1
         System.out.println("Hello World!");
     }
 }
